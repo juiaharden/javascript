bd4e670cd361662d7d00f8e799b55443306919e9
juiaharden <130366314+juiaharden@users.noreply.github.com>
2023-09-11 08:49:00 -0300

    Add files via upload

--- introducao-javascript/js/principal.js
+++ introducao-javascript/js/principal.js
@@ -1,1 +1,13 @@
-console.log("snhhhhhh");
+//console.log("snhhhhhh");
+
+//---------------Daqui para baixo começa o código
+
+//alteração do titulo
+//console.log(document);
+//cria a variável titulo e armazena o conteudo da classe titulo nela
+let titulo = document.querySelector(".titulo");
+//console.log(titulo);
+//atribuição de um novo valor na variável titulo
+titulo.textContent = "Fake Natty Nutrição!";
+
+//alteração da tabela
